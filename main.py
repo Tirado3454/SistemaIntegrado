33873f1f6edc5287e646845ee035ed75be16cc08
Tirado3454 <augustotirado@gmail.com>
2024-12-22 22:02:25 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
+import streamlit as st
+from utils.maintexto import mhd_function
+from utils.tabuleiro import board_editor_function
+from utils.frases import phrase_bank_function
+from utils.export import generate_pdf, generate_csv
+
+# Configuração inicial da página
+st.set_page_config(page_title="Ensino de Ciência e Xadrez", layout="wide")
+
+# Definição do menu de navegação
+menu_option = st.sidebar.radio(
+    "Escolha uma funcionalidade:",
+    ["Modelo Hipotético-Dedutivo", "Editor de Tabuleiro", "Banco de Frases", "Exportar Dados"]
+)
+
+# Lógica para cada opção do menu
+if menu_option == "Modelo Hipotético-Dedutivo":
+    mhd_function()
+elif menu_option == "Editor de Tabuleiro":
+    board_editor_function()
+elif menu_option == "Banco de Frases":
+    phrase_bank_function()
 elif menu_option == "Exportar Dados":
     st.title("Exportar Dados Consolidado")
     st.markdown("### Dados do Modelo Hipotético-Dedutivo")
