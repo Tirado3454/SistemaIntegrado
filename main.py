c9ce49ecd4d9e20ab588f0f92dc183fde18b6824
Tirado3454 <augustotirado@gmail.com>
2024-12-22 21:38:44 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,45 +1,33 @@
-import streamlit as st
-from utils.maintexto import mhd_function
-from utils.tabuleiro import board_editor_function
-from utils.frases import phrase_bank_function
-from utils.export import generate_pdf, generate_csv
-
-st.set_page_config(page_title="Ensino de Ciência e Xadrez", layout="wide")
-
-# Menu de navegação
-menu_option = st.sidebar.radio(
-    "Escolha uma funcionalidade:",
-    ["Modelo Hipotético-Dedutivo", "Editor de Tabuleiro", "Banco de Frases", "Exportar Dados"]
-)
-
-# Dados globais
-if "mhd_data" not in st.session_state:
-    st.session_state.mhd_data = {}
-if "board_data" not in st.session_state:
-    st.session_state.board_data = ""
-if "phrases_selected" not in st.session_state:
-    st.session_state.phrases_selected = []
-
-# Navegação entre módulos
-if menu_option == "Modelo Hipotético-Dedutivo":
-    mhd_function()
-elif menu_option == "Editor de Tabuleiro":
-    board_editor_function()
-elif menu_option == "Banco de Frases":
-    phrase_bank_function()
 elif menu_option == "Exportar Dados":
     st.title("Exportar Dados Consolidado")
     st.markdown("### Dados do Modelo Hipotético-Dedutivo")
-    st.write(st.session_state.mhd_data)
+    if st.session_state.mhd_data:
+        st.write(st.session_state.mhd_data)
+    else:
+        st.warning("Nenhum dado do Modelo Hipotético-Dedutivo disponível.")
+
     st.markdown("### Configuração do Tabuleiro")
-    st.write(st.session_state.board_data)
+    if st.session_state.board_data:
+        st.write(st.session_state.board_data)
+    else:
+        st.warning("Nenhuma configuração de tabuleiro disponível.")
+
     st.markdown("### Frases Selecionadas")
-    st.write(st.session_state.phrases_selected)
+    if st.session_state.phrases_selected:
+        st.write(st.session_state.phrases_selected)
+    else:
+        st.warning("Nenhuma frase selecionada.")
 
     export_format = st.radio("Escolha o formato de exportação:", ["PDF", "CSV"])
 
     if st.button("Exportar"):
         if export_format == "PDF":
-            generate_pdf(st.session_state.mhd_data, st.session_state.board_data, st.session_state.phrases_selected)
+            if st.session_state.mhd_data or st.session_state.board_data or st.session_state.phrases_selected:
+                generate_pdf(st.session_state.mhd_data, st.session_state.board_data, st.session_state.phrases_selected)
+            else:
+                st.warning("Não há dados disponíveis para exportar.")
         elif export_format == "CSV":
-            generate_csv(st.session_state.mhd_data, st.session_state.board_data, st.session_state.phrases_selected)
+            if st.session_state.mhd_data or st.session_state.board_data or st.session_state.phrases_selected:
+                generate_csv(st.session_state.mhd_data, st.session_state.board_data, st.session_state.phrases_selected)
+            else:
+                st.warning("Não há dados disponíveis para exportar.")
